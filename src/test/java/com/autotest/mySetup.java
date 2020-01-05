c5460bc53e2cc287094b900ce630638e7dc71c13
artyom369 <art.maksimenko.1983@gmail.com>
2020-01-05 03:20:22 +0200

    update createCommunity Test

--- src/test/java/com/autotest/mySetup.java
+++ src/test/java/com/autotest/mySetup.java
@@ -1,10 +1,13 @@
 package com.autotest;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Locatable;
+import org.openqa.selenium.support.ui.Select;
+
 import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
 
@@ -25,7 +28,7 @@ public class mySetup extends WebDriverSettings {
         driver.findElement(By.id("community_group_sortOrder")).sendKeys("1");
         //driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 
-        // Scroll to element
+            // Scroll to element
         ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView();",driver.findElement(By.cssSelector(".btn.btn-primary")));
         driver.findElement(By.cssSelector(".btn.btn-primary")).click();
         driver.findElement(By.cssSelector(".btn.btn-primary")).click();
@@ -38,12 +41,23 @@ public class mySetup extends WebDriverSettings {
 
         loginAsSuperAdmin();
 
-        // Create community
+            // Create community
         driver.get("https://feature-art.rallyware.com/management/communities");
-        driver.findElement(By.cssSelector(".controls__item")).click();
+        driver.findElement(By.linkText("Create community")).click();
+        //driver.findElement(By.cssSelector(".controls__item")).click();
         driver.findElement(By.id("community_title")).sendKeys("Art Community");
-
         driver.findElement(By.id("community_description_ifr")).sendKeys(" Test");
+        driver.findElement(By.id("community_communityGroup")).click();
+        Select select = new Select(driver.findElement(By.id("community_communityGroup")));
+        select.selectByVisibleText("Artyom User Group");
+        driver.findElement(By.cssSelector(".custom-check-label > .custom-item")).click();
+        driver.findElement(By.cssSelector(".btn.btn-primary")).click();
+
+        String bodyText = driver.findElement(By.cssSelector(".item--overflow")).getText();
+        Assert.assertTrue("Text not found!", bodyText.contains("Art Community"));
+
+
+
 
     }
 
