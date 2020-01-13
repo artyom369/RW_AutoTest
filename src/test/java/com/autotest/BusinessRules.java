7159a9cf1aa4dca14120946fadfac3437f4c51b6
artyom369 <art.maksimenko.1983@gmail.com>
2020-01-13 18:00:14 +0200

    business rules update

--- src/test/java/com/autotest/BusinessRules.java
+++ src/test/java/com/autotest/BusinessRules.java
@@ -14,12 +14,43 @@ public class BusinessRules extends WebDriverSettings {
         driver.get("https://feature-art.rallyware.com/management/levels");
         driver.findElement(By.cssSelector(".controls__item")).click();
 
+        // dropDown Trigger list
         driver.findElement(By.cssSelector(".rules-title > .collapse-trigger")).click();
         driver.findElement(By.cssSelector(".add-rule-trigger")).click();
-        // dropDown list
+
+        //  Trigger [System] New month
+        Select selectNewMonth = new Select(driver.findElement(By.cssSelector(".unchosen-trigger")));
+        selectNewMonth.selectByValue("core.common.new_month");
+
+        //  Trigger [System] New day
+        Select selectNewDay = new Select(driver.findElement(By.cssSelector(".unchosen-trigger")));
+        selectNewDay.selectByValue("core.common.new_day");
+
+        //  Trigger [Task] Task successful
+        Select selectTaskSuccessful = new Select(driver.findElement(By.cssSelector(".unchosen-trigger")));
+        selectTaskSuccessful.selectByValue("ext.task.completed");
+
+        //  Trigger [Task] Task failed
+        Select selectTaskFailed = new Select(driver.findElement(By.cssSelector(".unchosen-trigger")));
+        selectTaskFailed.selectByValue("ext.task.failed");
+
+        // Trigger [User] Avatar upload
         Select selectTriggerAvatar = new Select(driver.findElement(By.cssSelector(".unchosen-trigger")));
         //selectTriggerAvatar.selectByVisibleText("[User] Avatar upload");
         selectTriggerAvatar.selectByValue("core.user.business_rule.avatar.initial_upload");
+
+        //  Trigger [User] Custom attribute updated
+        Select selectCustomAttributeUpdated = new Select(driver.findElement(By.cssSelector(".unchosen-trigger")));
+        selectCustomAttributeUpdated.selectByValue("core.user.custom_attr.updated");
+
+        //  Trigger [User] Points Awarded
+        Select selectPointsAwarded = new Select(driver.findElement(By.cssSelector(".unchosen-trigger")));
+        selectPointsAwarded.selectByValue("core.user.points.awarded");
+
+        //  Trigger [Level] Level reached
+        Select selectLevelReached = new Select(driver.findElement(By.cssSelector(".unchosen-trigger")));
+        selectLevelReached.selectByValue("ext.level.business_rule.trigger.level_achieved");
+
 
     }
 }
